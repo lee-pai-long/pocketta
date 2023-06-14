2bfce26ff636b1e58fc5afb271d4c93c9b3011c7
lpl <mohamed.ali.saidina+github@gmail.com>
2023-06-15 00:24:11 +0200

    Add main script

---
+++ main.js
@@ -0,0 +1,52 @@
+import fs from 'fs';
+
+import { program  as cli } from 'commander';
+
+import { extract_urls_from_tabs_file } from './src/core.js';
+import { save_urls_to_pocket } from './src/core.js';
+
+
+const DEFAULT_TABS_FILE_PATH = './tabs.csv'
+
+
+function main() {
+
+    cli 
+        .name('pocketta')
+        .description('Save multiple urls to pocket.com at once')
+        .version(
+            fs.readFileSync('./.node-version')
+                .toString()
+                .split('\n')[0]
+        )
+        .option('-f, --file <file>', 'path of the tabs file')
+    ;
+    cli.parse();
+
+    const options = cli.opts();
+    const tabs_file_path = options.file ? options.file : DEFAULT_TABS_FILE_PATH;
+
+    let urls = []
+    try {
+        urls = extract_urls_from_tabs_file(tabs_file_path)
+    } catch (e) {
+        console.log(e.message)
+        process.exit(1)
+    }
+
+    save_urls_to_pocket(urls)
+
+    try {
+        save_urls_to_pocket(urls)
+    } catch (e) {
+        console.log(e)
+        process.exit(1)
+    }
+
+    process.exit(0)
+}
+
+
+main();
+
+
